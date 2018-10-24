1db60965d6c651652ab7898f134cd80149815306
Jay Butera <buterajay@gmail.com>
2018-10-24 16:34:44 -0400

    Compiles

--- contracts/QueueDelegate.sol
+++ contracts/QueueDelegate.sol
@@ -1,20 +1,16 @@
 import './IDelegate.sol';
 
 contract QueueDelegate is IDelegate {
-    struct Delegate {
-        address addr;
-        uint256 amount;
-    }
-
-    //Delegate[] delegates;
 
     function join (uint256 stake_amount) public {
+        /*
         Delegate memory d = Delegate({
             addr: msg.sender,
             amount: stake_amount
         });
+        */
         //delegates.push(d);
-        add(d);
+        add(msg.sender, stake_amount);
     }
 
     function withdraw () public {
@@ -22,46 +18,43 @@ contract QueueDelegate is IDelegate {
     }
 
     function withdraw_some (uint256 stake_amount) public {
-        if ( delegates[ bytes32(msg.sender) ].info.amount <= stake_amount )
+        if ( delegates[ msg.sender ].amount <= stake_amount )
             remove( msg.sender );
         else
-            delegates[ bytes32(msg.sender) ].info.amount -= stake_amount; // Make this safe subtract
+            delegates[ msg.sender ].amount -= stake_amount; // Make this a safe subtract
     }
 
 
     //
     // Linked list impl
     //
-    struct Node {
-        bytes32 next;
-        bytes32 prev;
-        Delegate info;
+    struct Delegate {
+        address next;
+        address prev;
+        uint256 amount;
     }
 
     uint256 length = 0;
-    bytes32 public head;
-    mapping (address => Node) public delegates;
+    address public head;
+    mapping (address => Delegate) public delegates;
 
-    function add(Delegate d) private {
-        Node memory node = Node(head, d);
-        //bytes32 id = sha3(d.addr, now, d.amount);
-        bytes32 id;
-        address a = d.addr;
-        assembly {
-            id := mload(a)
-        }
-        //bytes32 id = bytes32(d.addr);
-        delegates[id] = node;
-        head       = id;
-        length     = length+1;
+    function add(address _addr, uint256 _amount) private {
+        delegates[_addr] = Delegate({
+            next: address(0),
+            prev: head,
+            amount: _amount
+        });
+
+        head = _addr;
+        length += 1;
     }
 
-    function remove(address addr) private {
-        Node storage n = delegates[ bytes32(addr) ];
-        Node next_id = n.next;
-        Node prev_id = n.prev;
+    function remove(address _addr) private {
+        Delegate storage d = delegates[_addr];
+        address next_id = d.next;
+        address prev_id = d.prev;
 
         delegates[prev_id].next = next_id;
-        delete n;
+        delete delegates[_addr];
     }
 }
