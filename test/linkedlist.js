e2d3f8cfea261115083b3619ad50c5f15b9ee6dc
Jay Butera <buterajay@gmail.com>
2018-10-25 21:32:32 -0400

    Fix test to mint

--- test/linkedlist.js
+++ test/linkedlist.js
@@ -6,13 +6,13 @@ contract('Linked list test', accounts => {
    let token
 
    it('Should add 3 stakers to list', async () => {
-      delegate = await QD.new();
       token    = await BurnableERC20.new(1000, { from:accounts[0] })
+      delegate = await QD.new(token.address)
 
       // Give stakers tokens
-      token.transfer(accounts[1], 100, { from: accounts[0] })
-      token.transfer(accounts[2], 100, { from: accounts[0] })
-      token.transfer(accounts[3], 100, { from: accounts[0] })
+      token.mint(accounts[1], 100, { from: accounts[0] })
+      token.mint(accounts[2], 100, { from: accounts[0] })
+      token.mint(accounts[3], 100, { from: accounts[0] })
 
       // Approve delegate staking
       token.approve(delegate.address, 100, { from: accounts[1] })
@@ -24,11 +24,11 @@ contract('Linked list test', accounts => {
       delegate.join(50,  { from: accounts[2] })
       delegate.join(25,  { from: accounts[3] })
 
+      // Checks
+      assert.equal(await delegate.length(), 3)
       assert.equal( (await delegate.get(accounts[1])).toNumber(), 100)
       assert.equal( (await delegate.get(accounts[2])).toNumber(), 50)
       assert.equal( (await delegate.get(accounts[3])).toNumber(), 25)
-
-      assert.equal(await delegate.length(), 3)
    })
 
    it('Should remove 3 stakers from list', async () => {
