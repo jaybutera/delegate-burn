be294fc3606bb1375d4a600dc0654f3ac1804b1b
Jay Butera <buterajay@gmail.com>
2018-11-12 14:30:44 -0600

    First test works

--- contracts/QueueDelegate.sol
+++ contracts/QueueDelegate.sol
@@ -5,8 +5,8 @@ import './IDelegate.sol';
 import './PoB/BurnableERC20.sol';
 import './PoB/BurnableStakeBank.sol';
 
-contract QueueDelegate is IDelegate {
-    mapping (address => Delegate) public delegates;
+contract QueueDelegate {// is IDelegate {
+    mapping (address => Staker) public stakers;
     //BurnableERC20 token;
     BurnableStakeBank bsb;
 
@@ -18,7 +18,7 @@ contract QueueDelegate is IDelegate {
     // Returns address of staker that was burned for
     function burnAllForNext (bytes __data) public returns (address) {
         address headCpy = head; // Copy head because it will change after burn
-        bsb.burn( delegates[headCpy].amount, __data );
+        burn( stakers[headCpy].amount, __data );
         return headCpy;
     }
 
@@ -26,8 +26,8 @@ contract QueueDelegate is IDelegate {
         bsb.burnFor(head, amount, __data); // TODO: specify token name in data
 
         // If all is burned from staker, remove from queue
-        if ( amount >= delegates[head].amount ) {
-            address tmp = head.next; // Temporary storage for new head ptr
+        if ( amount >= stakers[head].amount ) {
+            address tmp = stakers[head].next; // Temporary storage for new head ptr
             remove(head);            // Remove current head node
             head = tmp;              // Replace head ptr
         }
@@ -36,44 +36,46 @@ contract QueueDelegate is IDelegate {
     function join (uint256 stake_amount, bytes token_id) public {
         // Transfer staker funds to delegate account
         // token.transferFrom(msg.sender, this, stake_amount);
-        bsb.stake(msg.sender, stake_amount, token_id);
+        bsb.stakeFor(msg.sender, stake_amount, token_id);
 
-        // Add staker to burn list
+        // Add staker to bstake urn list
         add(msg.sender, stake_amount);
     }
 
-    function withdraw () public {
-        //require( delegates[msg.sender].exists == true );
+    function withdraw (bytes token_id) public {
+        //require( stakers[msg.sender].exists == true );
 
-        withdraw_some( delegates[msg.sender].amount );
+        withdraw_some( stakers[msg.sender].amount, token_id );
     }
 
-    function withdraw_some (uint256 stake_amount) public {
-        //require( delegates[msg.sender].exists == true );
-        require( delegates[msg.sender].amount >= stake_amount );
+    function withdraw_some (uint256 stake_amount, bytes token_id) public {
+        //require( stakers[msg.sender].exists == true );
+        require( stakers[msg.sender].amount >= stake_amount );
 
         // Return staker's money
-        token.transfer(msg.sender, stake_amount);
+        bsb.unstake(stake_amount, token_id);
+
+        // TODO: Should not track stake amount in QD contract since it's already tracked in BSB
 
         // Subtract amount or remove staker from burn list altogether
-        if ( delegates[ msg.sender ].amount <= stake_amount )
+        if ( stakers[ msg.sender ].amount <= stake_amount )
             remove( msg.sender );
         else
-            delegates[ msg.sender ].amount -= stake_amount; // Make this a safe subtract
+            stakers[ msg.sender ].amount -= stake_amount; // Make this a safe subtract
 
         // Decrement
         length -= 1;
     }
 
     function get (address a) view public returns (uint256) {
-        return delegates[a].amount;
+        return stakers[a].amount;
     }
 
 
     //
     // Linked list impl
     //
-    struct Delegate {
+    struct Staker {
         address next;
         address prev;
         uint256 amount;
@@ -84,7 +86,7 @@ contract QueueDelegate is IDelegate {
     address head;
 
     function add(address _addr, uint256 _amount) private {
-        delegates[_addr] = Delegate({
+        stakers[_addr] = Staker({
             next: address(0),
             prev: head,
             amount: _amount,
@@ -96,12 +98,12 @@ contract QueueDelegate is IDelegate {
     }
 
     function remove(address _addr) private {
-        Delegate storage d = delegates[_addr];
+        Staker storage d = stakers[_addr];
         address next_id = d.next;
         address prev_id = d.prev;
 
-        delegates[prev_id].next = next_id;
-        delete delegates[_addr];
+        stakers[prev_id].next = next_id;
+        delete stakers[_addr];
     }
 }
 
--- test/linkedlist.js
+++ test/linkedlist.js
@@ -1,13 +1,21 @@
 const QD = artifacts.require('QueueDelegate')
+const BSB = artifacts.require('BurnableStakeBank')
+const TR  = artifacts.require('TokenRegistry')
 const BurnableERC20 = artifacts.require('BurnableERC20')
 
 contract('Linked list test', accounts => {
    let delegate
    let token
+   let bsb
+   let tr
 
    it('Should add 3 stakers to list', async () => {
-      token    = await BurnableERC20.new(1000, { from:accounts[0] })
-      delegate = await QD.new(token.address)
+      token    = await BurnableERC20.new(1000)
+      tr       = await TR.new()
+      bsb      = await BSB.new(tr.address, 1)
+      delegate = await QD.new(bsb.address)
+
+      await tr.setToken('test', token.address)
 
       // Give stakers tokens
       token.mint(accounts[1], 100, { from: accounts[0] })
@@ -15,14 +23,14 @@ contract('Linked list test', accounts => {
       token.mint(accounts[3], 100, { from: accounts[0] })
 
       // Approve delegate staking
-      token.approve(delegate.address, 100, { from: accounts[1] })
-      token.approve(delegate.address, 100, { from: accounts[2] })
-      token.approve(delegate.address, 100, { from: accounts[3] })
+      token.approve(bsb.address, 100, { from: accounts[1] })
+      token.approve(bsb.address, 100, { from: accounts[2] })
+      token.approve(bsb.address, 100, { from: accounts[3] })
 
       // Join delegate staking
-      delegate.join(100, { from: accounts[1] })
-      delegate.join(50,  { from: accounts[2] })
-      delegate.join(25,  { from: accounts[3] })
+      delegate.join(100, 'test', { from: accounts[1] })
+      delegate.join(50, 'test',  { from: accounts[2] })
+      delegate.join(25, 'test',  { from: accounts[3] })
 
       // Checks
       assert.equal(await delegate.length(), 3)
@@ -32,9 +40,9 @@ contract('Linked list test', accounts => {
    })
 
    it('Should remove 3 stakers from list', async () => {
-      delegate.withdraw({from: accounts[1]})
-      delegate.withdraw({from: accounts[2]})
-      delegate.withdraw({from: accounts[3]})
+      delegate.withdraw('test', {from: accounts[1]})
+      delegate.withdraw('test', {from: accounts[2]})
+      delegate.withdraw('test', {from: accounts[3]})
 
       assert.equal(await delegate.length(), 0)
    })
